ca0fb7f48ce3bcb0f38b53a280b6f8cd1ceed874
lelecatica <malanhuk.leticia@escola.pr.gov.br>
2026-08-06 13:18:45 -0300

    y

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
+const caixaresultado = document.querySelector(".caixa-resultado");
+const textoresultado = document.querySelector(".textoresultado");
+
+const perguntas = [
+    {
+   enunciado: "pergunta 1",
+   alternativas: [
+    "alternativa1", 
+    "alternativa 2"
+    ]
+ },
+    {
+   enunciado: "pergunta 1",
+   alternativas: [
+    "alternativa1", 
+    "alternativa 2"
+    ]
+ }
+
+    ]
